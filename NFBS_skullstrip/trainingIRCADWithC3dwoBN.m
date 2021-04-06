fe3d880242a1bcd172d28a375050bcf1d486d19d
btzolovaRice <bmt3@rice.edu>
2021-04-06 14:29:00 -0500

    Committing training files with and w/o batch normalization

--- NFBS_skullstrip/trainingIRCADWithC3dwoBN.m
+++ NFBS_skullstrip/trainingIRCADWithC3dwoBN.m
@@ -40,7 +40,7 @@ num_images = length(procVolDs.Labels); %number of obervations for kfold
 c1 = cvpartition(num_images,'kfold',5); %try with 4 instead of 5 
 err = zeros(c1.NumTestSets,1);
 
-C = cell(1, 5);
+C = cell(1, c1.NumTestSets);
 [idxTest] = deal(C);
 
 for idxFold = 1:c1.NumTestSets 
@@ -90,13 +90,11 @@ lgraph = layerGraph();
 
 tempLayers = [
     image3dInputLayer([64 64 64 n_layer],"Name","input","Normalization","none")
-    batchNormalizationLayer("Name","BN_Module1_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module1_Level1","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module1_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module1_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module1_Level2","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module1_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
@@ -106,13 +104,11 @@ lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     maxPooling3dLayer([2 2 2],"Name","maxpool_Module1","Padding","same","Stride",[2 2 2])
-    batchNormalizationLayer("Name","BN_Module2_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module2_Level1","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module2_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module2_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module2_Level2","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module2_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
@@ -122,13 +118,11 @@ lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     maxPooling3dLayer([2 2 2],"Name","maxpool_Module2","Padding","same","Stride",[2 2 2])
-    batchNormalizationLayer("Name","BN_Module3_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module3_Level1","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module3_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module3_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module3_Level2","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module3_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
@@ -138,13 +132,11 @@ lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     maxPooling3dLayer([2 2 2],"Name","maxpool_Module3","Padding","same","Stride",[2 2 2])
-    batchNormalizationLayer("Name","BN_Module4_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module4_Level1","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module4_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module4_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module4_Level2","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module4_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
@@ -156,13 +148,11 @@ lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     concatenationLayer(4,2,"Name","concat3")
-    batchNormalizationLayer("Name","BN_Module5_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module5_Level1","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module5_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module5_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module5_Level2","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module5_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
@@ -174,13 +164,11 @@ lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     concatenationLayer(4,2,"Name","concat2")
-    batchNormalizationLayer("Name","BN_Module6_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module6_Level1","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module6_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module6_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module6_Level2","Padding","same","WeightsInitializer","narrow-normal")
     reluLayer("Name","relu_Module6_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
@@ -192,20 +180,17 @@ lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     concatenationLayer(4,2,"Name","concat1")
-    batchNormalizationLayer("Name","BN_Module7_Level1")
     convolution3dLayer([3 3 3],16,"Name","conv_Module7_Level1","Padding","same")
     reluLayer("Name","relu_Module7_Level1")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
-    batchNormalizationLayer("Name","BN_Module7_Level2")
     convolution3dLayer([3 3 3],16,"Name","conv_Module7_Level2","Padding","same")
     reluLayer("Name","relu_Module7_Level2")];
 lgraph = addLayers(lgraph,tempLayers);
 
 tempLayers = [
     concatenationLayer(4,2,"Name","concat")
-    batchNormalizationLayer("Name","BN_Module7_Level3")
     convolution3dLayer([1 1 1],2,"Name","ConvLast_Module7")
     softmaxLayer("Name","softmax")
     dicePixelClassification3dLayer("output")];
@@ -216,34 +201,34 @@ clear tempLayers;
 %% Connect Layer Branches
 % Connect all the branches of the network to create the network graph.
 
-lgraph = connectLayers(lgraph,"relu_Module1_Level1","BN_Module1_Level2");
+lgraph = connectLayers(lgraph,"relu_Module1_Level1","conv_Module1_Level2");
 lgraph = connectLayers(lgraph,"relu_Module1_Level1","concat_1/in1");
 lgraph = connectLayers(lgraph,"relu_Module1_Level2","concat_1/in2");
 lgraph = connectLayers(lgraph,"concat_1","maxpool_Module1");
 lgraph = connectLayers(lgraph,"concat_1","concat1/in1");
-lgraph = connectLayers(lgraph,"relu_Module2_Level1","BN_Module2_Level2");
+lgraph = connectLayers(lgraph,"relu_Module2_Level1","conv_Module2_Level2");
 lgraph = connectLayers(lgraph,"relu_Module2_Level1","concat_2/in1");
 lgraph = connectLayers(lgraph,"relu_Module2_Level2","concat_2/in2");
 lgraph = connectLayers(lgraph,"concat_2","maxpool_Module2");
 lgraph = connectLayers(lgraph,"concat_2","concat2/in1");
-lgraph = connectLayers(lgraph,"relu_Module3_Level1","BN_Module3_Level2");
+lgraph = connectLayers(lgraph,"relu_Module3_Level1","conv_Module3_Level2");
 lgraph = connectLayers(lgraph,"relu_Module3_Level1","concat_3/in1");
 lgraph = connectLayers(lgraph,"relu_Module3_Level2","concat_3/in2");
 lgraph = connectLayers(lgraph,"concat_3","maxpool_Module3");
 lgraph = connectLayers(lgraph,"concat_3","concat3/in1");
-lgraph = connectLayers(lgraph,"relu_Module4_Level1","BN_Module4_Level2");
+lgraph = connectLayers(lgraph,"relu_Module4_Level1","conv_Module4_Level2");
 lgraph = connectLayers(lgraph,"relu_Module4_Level1","concat_4/in1");
 lgraph = connectLayers(lgraph,"relu_Module4_Level2","concat_4/in2");
 lgraph = connectLayers(lgraph,"upsample_Module4","concat3/in2");
-lgraph = connectLayers(lgraph,"relu_Module5_Level1","BN_Module5_Level2");
+lgraph = connectLayers(lgraph,"relu_Module5_Level1","conv_Module5_Level2");
 lgraph = connectLayers(lgraph,"relu_Module5_Level1","concat_5/in1");
 lgraph = connectLayers(lgraph,"relu_Module5_Level2","concat_5/in2");
 lgraph = connectLayers(lgraph,"upsample_Module5","concat2/in2");
-lgraph = connectLayers(lgraph,"relu_Module6_Level1","BN_Module6_Level2");
+lgraph = connectLayers(lgraph,"relu_Module6_Level1","conv_Module6_Level2");
 lgraph = connectLayers(lgraph,"relu_Module6_Level1","concat_6/in1");
 lgraph = connectLayers(lgraph,"relu_Module6_Level2","concat_6/in2");
 lgraph = connectLayers(lgraph,"upsample_Module6","concat1/in2");
-lgraph = connectLayers(lgraph,"relu_Module7_Level1","BN_Module7_Level2");
+lgraph = connectLayers(lgraph,"relu_Module7_Level1","conv_Module7_Level2");
 lgraph = connectLayers(lgraph,"relu_Module7_Level1","concat/in2");
 lgraph = connectLayers(lgraph,"relu_Module7_Level2","concat/in1");
 
